aa2929ba68805db0badd6327dfbbfd6882770981
DavidDG21 <los.pc.13@gmail.com>
2022-07-01 20:06:14 -0500

    PRueba del cloud

---
+++ logica/conexion.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = "bxm1bnwsrap9dx051kpk-mysql.services.clever-cloud.com";
+$user = "ujewckapmr3ufoz6";
+$clave = "9Y0tAYheBNtw5LyCgtsc";
+$bd  = "bxm1bnwsrap9dx051kpk";
+
+$conectar = mysqli_connect($host,$user,$clave,$bd);
+
+
+
+
+?>
